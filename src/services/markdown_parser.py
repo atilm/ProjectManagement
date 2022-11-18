726c15545ca6d21239a00841197fa3ed134b2e43
Andreas Thielmann <thielmann.andreas@googlemail.com>
2022-11-18 22:51:26 +0100

    parsing markdown tables

--- src/services/markdown_parser.py
+++ src/services/markdown_parser.py
@@ -1,3 +1,10 @@
+import re
+
+class TableRowException(Exception):
+    def __init__(self, lineNumber : int, *args: object) -> None:
+        super().__init__(*args)
+        self.message = f"No matching table header in line {lineNumber}"
+
 class MarkdownDocument:
     def __init__(self) -> None:
         self.content = []
@@ -9,31 +16,91 @@ class MarkdownDocument:
         self.content.append(object)
 
 class MarkdownSection:
-    def __init__(self, title : str) -> None:
+    def __init__(self, title : str, lineNumber : int) -> None:
         self.title = title
+        self.lineNumber = lineNumber
+
+class MarkdownTableRow:
+    def __init__(self, cells : list, lineNumber : int) -> None:
+        self._cells = cells
+        self.lineNumber = lineNumber
+
+    def getColumnCount(self) -> int:
+        return len(self._cells)
+
+    def get(self, columnIdx : int):
+        return self._cells[columnIdx]
 
 class MarkdownTable:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, headerRow : MarkdownTableRow) -> None:
+        self._headerRow = headerRow
+        self.rows = []
+
+    def getColumnCount(self) -> int:
+        return self._headerRow.getColumnCount()
+
+    def getColumnHeader(self, columnIndex : int) -> str:
+        return self._headerRow.get(columnIndex)
+    
+    def getRowCount(self) -> int:
+        return len(self.rows)
 
 
 class MarkdownParser:
     def __init__(self) -> None:
-        pass
+        self._tableRowRegex = re.compile(r"\|(.+\|)+")
+        self._tableSeparatorRegex = re.compile(r"\|([\s-]+\|)+")
+        self._state = self._parsingDocumentState
 
     def parse(self, input : str) -> MarkdownDocument:
-        document = MarkdownDocument()
+        self._document = MarkdownDocument()
+        self._currentTableItem = None
 
         lines = input.splitlines()
 
-        for line in lines:
-            if self._isSection(line):
-                document.append(self._createSection(line))
+        previousLine = None
 
-        return document
+        for lineIdx, line in enumerate(lines):
+            lineNumber = lineIdx + 1
+            self._state(line, lineNumber, previousLine)
+            previousLine = line
 
-    def _isSection(self, line : str):
+        if self._currentTableItem != None:
+            self._document.append(self._currentTableItem)
+
+        return self._document
+
+    def _parsingDocumentState(self, line : str, lineNumber : int, previousLine : str):
+        if self._isSection(line):
+            self._document.append(self._createSection(line, lineNumber))
+        elif self._isTableSeparator(line):
+            self._currentTableItem = MarkdownTable(self._parseTableRow(previousLine, lineNumber - 1))
+            self._state = self._parsingTableState
+
+    def _parsingTableState(self, line : str, lineNumber : int, previousLine : str):
+        if not line:
+            self._document.append(self._currentTableItem)
+            self._currentTableItem = None
+            self._state = self._parsingDocumentState
+        else:
+            self._currentTableItem.rows.append(self._parseTableRow(line, lineNumber))
+
+    def _isSection(self, line : str) -> bool:
         return line.startswith('#')
 
-    def _createSection(self, line : str):
-        return MarkdownSection(line.strip().strip("#").strip())
+    def _isTableRow(self, line : str) -> bool:
+        return self._tableRowRegex.match(line) != None
+
+    def _parseTableRow(self, line : str, lineNumber : int) -> list:
+        if not self._isTableRow(line):
+            raise TableRowException(lineNumber)
+
+        rawEntries = line.strip().strip("|").split("|")
+        strippedEntries = [e.strip() for e in rawEntries]
+        return MarkdownTableRow(strippedEntries, lineNumber)
+
+    def _isTableSeparator(self, line : str) -> bool:
+        return self._tableSeparatorRegex.match(line) != None
+
+    def _createSection(self, line : str, lineNumber : int):
+        return MarkdownSection(line.strip().strip("#").strip(), lineNumber)
